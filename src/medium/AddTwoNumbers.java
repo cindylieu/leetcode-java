fef5e6aaca4b719993c0c348ea8999c8c4a1e274
Cindy Lieu <Cindy Lieu>
2021-07-19 19:18:44 -0400

    add two numbers

--- src/medium/AddTwoNumbers.java
+++ src/medium/AddTwoNumbers.java
@@ -1,18 +1,47 @@
 package medium;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.util.List;
+
+//Definition for singly-linked list.
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
 
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+    }
 
+    static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry > 0 ) {
+            int sum = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+
+            temp.next = node;
+            temp = temp.next;
+        }
+        return head.next;
     }
 }
